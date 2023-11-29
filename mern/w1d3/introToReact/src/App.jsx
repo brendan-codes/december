ed2f5fc53382338f0cc900a82dde7c73c7babdba
Brendan Stanton <bmsenate@gmail.com>
2023-11-29 09:40:33 -0800

    add wednesday lecture code

---
+++ mern/w1d3/introToReact/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Hello Worlds!</h1>
+
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt laudantium eligendi dignissimos sunt vel fugiat maxime possimus eaque assumenda, facere ex molestiae mollitia labore nostrum! Veritatis dicta officiis totam. Suscipit!</p>
+    </>
+  )
+}
+
+export default App
